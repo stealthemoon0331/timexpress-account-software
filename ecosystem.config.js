6fad534836a07e6513de488a06f0c7959ef7924e
stealthemoon0331 <kijimatakuma0331@gmail.com>
2025-06-07 06:15:27 +0900

    added ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "account-app-prod",
+      script: "npm",
+      args: "run start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    },
+    {
+      name: "account-app-staging",
+      script: "npm",
+      args: "run staging",
+      env: {
+        NODE_ENV: "staging",
+        PORT: 3001
+      }
+    }
+  ]
+};
